3ec15da7da29502d559b585577fa6c648a409e59
Mohammad Arif <codeinkotlang@gmail.com>
2025-06-23 11:27:20 +0530

    Refactor: Update Gradle and project settings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,15 +15,16 @@ android {
         applicationId = "com.synac.agecalculator"
         minSdk = 21
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
